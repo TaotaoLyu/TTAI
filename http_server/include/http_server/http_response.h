bb4a12128ab0375f5618d878bb053573d29918ab
Taotao Lyu <taotao.lyu@gmail.com>
2026-04-27 15:53:29 +0800

    improve code robustness with better error handling

--- http_server/include/http_server/http_response.h
+++ http_server/include/http_server/http_response.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <string>
-#include<sstream>
+#include <sstream>
 #include <unordered_map>
 
 namespace http
@@ -8,27 +8,27 @@ namespace http
     class HttpResponse
     {
     public:
-        operator std::string&()
+        operator std::string &()
         {
             std::ostringstream oss;
-            oss<<"HTTP/"<<version_<<" "<<status_<<" "<<describes_<<"\r\n";
-            headers_["Content-Length"]=std::to_string(body_.size());
-            for(auto& [key,value]:headers_)
+            oss << "HTTP/" << version_ << " " << status_ << " " << describes_ << "\r\n";
+            if (headers_.count("Content-Length") == 0)
+                headers_["Content-Length"] = std::to_string(body_.size());
+            for (auto &[key, value] : headers_)
             {
-                oss<<key<<": "<<value<<"\r\n";
+                oss << key << ": " << value << "\r\n";
             }
-            oss<<"\r\n";
-            oss<<body_;
-            message_ = std::move(oss.str()); 
+            oss << "\r\n";
+            oss << body_;
+            message_ = std::move(oss.str());
             return message_;
         }
-        std::string version_="1.1";
+        std::string version_ = "1.1";
         std::string status_;
         std::string describes_;
         std::unordered_map<std::string, std::string> headers_;
         std::string body_;
         std::string message_;
-
     };
 
 } // namespace http
